e297c688a6f34e12854a27713e968906e69367aa
Cristina Ferrer <krishee107@gmail.com>
2023-04-21 09:22:34 +0200

    Paginacion 2 y custom hook

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,30 +1,8 @@
-import { useEffect, useRef, useState } from "react"
-import { reqResApi } from "../api/reqRes"
-import { ReqResListado, Usuario } from "./interfaces/reqRes"
+import { useUsuarios } from "../hooks/useUsuarios"
+import { Usuario } from "./interfaces/reqRes";
 
 export const Usuarios = () => {
-    const [usuarios, setUsuarios] = useState<Usuario[]>([])
-    const paginaRef = useRef(1)
-
-    useEffect(() => {
-        cargarUsuarios();
-    }, [])
-
-    const cargarUsuarios = async () => {
-        const resp = await reqResApi.get<ReqResListado>('/users', {
-            params: {
-                page: paginaRef.current
-            }
-        })
-
-        if (resp.data.data.length > 0) {
-            setUsuarios(resp.data.data)
-            paginaRef.current++;
-        } else {
-            alert("No hay más registros");
-        }
-
-    }
+    const { usuarios, paginaAnterior, paginaSiguiente } = useUsuarios();
 
     const renderItem = ({ id, first_name, last_name, email, avatar }: Usuario) => {
         return (
@@ -56,7 +34,9 @@ export const Usuarios = () => {
                 </tbody>
             </table>
 
-            <button className="btn btn-primary" onClick={cargarUsuarios}>Siguiente</button>
+            <button className="btn btn-primary" onClick={paginaAnterior}>Anteriores</button>
+            &nbsp;
+            <button className="btn btn-primary" onClick={paginaSiguiente}>Siguiente</button>
         </>
     )
 }
